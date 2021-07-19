4292a27887f8f76a5c524fe88567c398155868e5
revanthkumaar <iamrevanthkumar@gmail.com>
2021-07-19 20:54:15 +0530

    final checkin for day3

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function selectCategory() {
+    console.log('select category function triggered');
   var selectCategory = document.getElementById(`homePageCategorySelection`);
   var selectedValue =
     selectCategory.options[selectCategory.selectedIndex].value;
