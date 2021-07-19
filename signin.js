213bd5af80c762ce16c6f6233fd39f9a958f46be
revanthkumaar <iamrevanthkumar@gmail.com>
2021-07-19 19:58:57 +0530

    Initial commit

---
+++ signin.js
@@ -0,0 +1,10 @@
+const a = 10;
+const b = 20;
+const c = a + b;
+console.log(c); //printing value to console
+document.getElementById("sumvalue").innerHTML = c; //printing value in website
+function displayDate() {
+  console.log(a);
+}
+
+
