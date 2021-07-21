632d8152cd388c8024fc814496a0255516bcd762
revanthkumaar <iamrevanthkumar@gmail.com>
2021-07-21 20:10:03 +0530

    final checkin day 4

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-function selectCategory() {
+
+
+const searchFunction =  (event) => {
+  
+  const searchElement = document.getElementById('search')
+  const searchWord = searchElement.value
+  console.log(searchWord)
+
+}
+
+
+const selectCategory = () => {
     console.log('select category function triggered');
   var selectCategory = document.getElementById(`homePageCategorySelection`);
   var selectedValue =
